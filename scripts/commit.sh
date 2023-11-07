9c51f743f83c0afec36974dd2df5319658560679
Jake Howard <git@theorangeone.net>
2023-11-07 19:27:20 +0000

    The commit script should just commit - not push

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -4,4 +4,3 @@ filename="$(date '+%Y/%Y-%m-%d.md')"
 curl https://hackmd.io/@racc/kit-team-agenda/download  | sed '/^---$/Q' > "$filename"
 git add "$filename"
 git commit -m "Add minutes for $(date '+%Y-%m-%d')"
-git push
